2565cad3d0560c53bf1191e74e3ac05b5af0eb9e
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-11-16 12:08:42 -0500

    Working list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
+    <cards v-bind:cards="cards"></cards>
   </div>
 </template>
 
 <script>
-
-
+import { MAGICAPI } from "./js/constants.js";
+import Cards from "./components/Cards";
+import axios from "axios";
 export default {
-  name: 'app',
-  components: {
-
+  name: "app",
+  components: { Cards },
+  data() {
+    return {
+      cards: "",
+      page: 1
+    };
+  },
+  methods: {
+    getCards: function() {
+      const URL = MAGICAPI.cards + "?page=" + this.page;
+      if (sessionStorage.getItem(URL) === null) {
+        axios
+          .get(URL)
+          .then(x => {
+            this.cards = x.data.cards;
+            // this.page++;
+            sessionStorage.setItem("cardPage", this.page);
+            sessionStorage.setItem(URL, JSON.stringify(x.data.cards));
+          })
+          .catch(x => console.log(x));
+      }else{
+        this.cards = JSON.parse(sessionStorage.getItem(URL));
+      }
+    }
+  },
+  beforeMount: function() {
+    if (sessionStorage.getItem("cardPage") === null) {
+      sessionStorage.setItem("cardPage", this.page);
+    } else {
+      this.page = sessionStorage.getItem("cardPage");
+    }
+    this.getCards();
   }
-}
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/Cards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cards">
+    <div class="card" v-for="card in cards" v-bind:key="card.id">
+        <h3>{{card.name}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cards',
+  props: ['cards'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.cards{
+    display: flex;
+    flex-wrap: wrap;
+    .card{
+        margin-right: 5px;
+    }
+}
+
+</style>
