4ea3f8f6ef9c5eb12ea73b0834aa30007687d573
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-11-16 17:13:28 -0500

    Implement next, previous

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
+    <a v-on:click="prev" v-if="headerLinks.prev !==undefined">Prev</a>
+    <a v-on:click="next">Next</a>
     <cards v-bind:cards="cards"></cards>
+    
   </div>
 </template>
 
@@ -8,30 +11,76 @@
 import { MAGICAPI } from "./js/constants.js";
 import Cards from "./components/Cards";
 import axios from "axios";
+var parse = require("parse-link-header");
 export default {
   name: "app",
   components: { Cards },
   data() {
     return {
       cards: "",
-      page: 1
+      headerLinks: ""
     };
   },
   methods: {
     getCards: function() {
-      const URL = MAGICAPI.cards + "?page=" + this.page;
+      const URL = MAGICAPI.cards;
       if (sessionStorage.getItem(URL) === null) {
         axios
           .get(URL)
           .then(x => {
             this.cards = x.data.cards;
+            this.headerLinks = parse(x.headers.link);
             // this.page++;
-            sessionStorage.setItem("cardPage", this.page);
+
             sessionStorage.setItem(URL, JSON.stringify(x.data.cards));
           })
           .catch(x => console.log(x));
-      }else{
+      } else {
         this.cards = JSON.parse(sessionStorage.getItem(URL));
+      }
+    },
+    next: function() {
+
+      if (sessionStorage.getItem(this.headerLinks.next.url) === null) {
+        axios
+          .get(this.headerLinks.next.url)
+          .then(x => {
+            this.cards = x.data.cards;
+            sessionStorage.setItem(
+              this.headerLinks.next.url,
+              JSON.stringify(x.data.cards)
+            );
+            this.headerLinks = parse(x.headers.link);
+            // this.page++;
+
+
+          })
+          .catch(x => console.log(x));
+      } else {
+        this.cards = JSON.parse(
+          sessionStorage.getItem(this.headerLinks.next.url)
+        );
+      }
+    },
+    prev: function() {
+      if (sessionStorage.getItem(this.headerLinks.prev.url) === null) {
+        axios
+          .get(this.headerLinks.prev.url)
+          .then(x => {
+            this.cards = x.data.cards;
+            sessionStorage.setItem(
+              this.headerLinks.prev.url,
+              JSON.stringify(x.data.cards)
+            );
+            this.headerLinks = parse(x.headers.link);
+            // this.page++;
+
+          })
+          .catch(x => console.log(x));
+      } else {
+        this.cards = JSON.parse(
+          sessionStorage.getItem(this.headerLinks.prev.url)
+        );
       }
     }
   },
